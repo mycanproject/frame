03526b7a7785b7d461cae594db1ea6e88ac01103
atom0502 <87877199+atom0502@users.noreply.github.com>
2021-11-26 00:20:39 +0900

    screenutil_ver

--- lib/main.dart
+++ lib/main.dart
@@ -1,35 +1,29 @@
 import 'package:flutter/material.dart';
 import 'package:framework/MainPages/MainPage.dart';
 import 'package:framework/frame/SettingPages/ChangeColor.dart';
+import 'package:framework/frame/Subject/SubjectData.dart';
 import '../frame/NewsList/NewsList.dart';
 import '../frame/SettingPages/ChangeColor.dart';
 import '../frame/TodoPages/TodoList.dart';
 import '../frame/Timetable/TimeTable.dart';
 import '../frame/Schedule/Schedule.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 void main() {
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'MyCan',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-      ),
-      home: Mycan(),
+    return ScreenUtilInit(
+      designSize: Size(100, 100),
+      builder: () => MaterialApp(
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+        ),
+      home: SubjectData(),
+      )
     );
   }
 }
@@ -49,8 +43,10 @@ class MycanState extends State<Mycan> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: selectPage(selecticon),
+      body: SafeArea(
+        child: Center(
+          child: selectPage(selecticon),
+        ),
       ),
       bottomNavigationBar: BottomNavigationBar(
         items: <BottomNavigationBarItem>[
